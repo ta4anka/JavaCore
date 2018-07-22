606e8e48e7fa96fcd48707406300cdb5bf610b8d
Vladymir <vladymirsheyko@gmail.com>
2018-07-22 23:44:42 +0300

    varargs: variable-length arguments

---
+++ src/main/java/net/proselyte/javacore/Schildt/Chapter_7/VarArgs.java
@@ -0,0 +1,28 @@
+package net.proselyte.javacore.Schildt.Chapter_7;
+
+// Demonstrate variable-length arguments
+public class VarArgs {
+    // vaTest() uses a vararg.
+    static void vaTest(int ... v) {
+        System.out.print("Number of args: " + v.length + " Contents: ");
+
+        for(int x : v)
+            System.out.print(x + " ");
+        System.out.println();
+    }
+
+    public static void main(String args[])
+    {
+// Notice how vaTest() can be called with a variable number of arguments.
+        vaTest(10);      // 1 arg
+        vaTest(1, 2, 3); // 3 args
+        vaTest();           // no args
+    }
+}
+
+
+/*
+Number of args: 1 Contents: 10
+Number of args: 3 Contents: 1 2 3
+Number of args: 0 Contents:
+ */
