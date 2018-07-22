253523c4eca2317fe6466b9ee7b0250540ddf214
Vladymir <vladymirsheyko@gmail.com>
2018-07-22 20:10:58 +0300

    the examples of nested and inner classes

---
+++ src/main/java/net/proselyte/javacore/Schildt/Chapter_7/InnerClassDemo2.java
@@ -0,0 +1,28 @@
+package net.proselyte.javacore.Schildt.Chapter_7;
+
+// This program will not compile.
+class Outer2 {
+    int outer_x = 100;
+    void test() {
+        Inner inner = new Inner();
+        inner.display();
+    }
+    //===== this is an inner class=========
+    class Inner {
+        int y = 10; // y is local to Inner
+        void display() {
+            System.out.println("display: outer_x = " + outer_x);
+        }
+    }
+    //=====================================
+
+    void showy() {
+//        System.out.println(y); // error, y not known here!
+    }
+}
+class InnerClassDemo2 {
+    public static void main(String args[]) {
+        Outer2 outer = new Outer2();
+        outer.test();
+    }
+}
