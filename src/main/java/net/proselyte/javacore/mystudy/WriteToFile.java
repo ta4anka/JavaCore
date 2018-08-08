3b3fef3e17e3880780976ef937709d73f558a3dc
Vladymir <vladymirsheyko@gmail.com>
2018-08-09 01:20:01 +0300

    an example of writing into text file

---
+++ src/main/java/net/proselyte/javacore/mystudy/WriteToFile.java
@@ -0,0 +1,16 @@
+package net.proselyte.javacore.mystudy;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class WriteToFile {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("testFile");
+        PrintWriter pw = new PrintWriter(file);
+        pw.println("Test row 1");
+        pw.println("Test row 2");
+
+        pw.close();
+    }
+}
