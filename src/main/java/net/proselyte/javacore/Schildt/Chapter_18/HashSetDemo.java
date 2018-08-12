bc3f4d755e545a76a0e23c8ba501830498303d6f
Vladymir <vladymirsheyko@gmail.com>
2018-08-12 20:20:37 +0300

    an example of HashSet

---
+++ src/main/java/net/proselyte/javacore/Schildt/Chapter_18/HashSetDemo.java
@@ -0,0 +1,21 @@
+package net.proselyte.javacore.Schildt.Chapter_18;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+    public static void main(String[] args) {
+        HashSet<String> hs = new HashSet<>();
+
+        hs.add("Beta");
+        hs.add("Alpha");
+        hs.add("Eta");
+        hs.add("Gamma");
+        hs.add("Epsilon");
+        hs.add("Omega");
+
+        System.out.println(hs);
+
+    }
+}
+
+//[Gamma, Eta, Alpha, Epsilon, Omega, Beta]
