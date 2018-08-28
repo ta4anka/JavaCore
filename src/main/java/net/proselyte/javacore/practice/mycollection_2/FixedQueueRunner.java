91ab92f634f4769682620dea975d3f8c73ee12f6
Vladymir <vladymirsheyko@gmail.com>
2018-08-28 19:33:53 +0300

    added exception

--- src/main/java/net/proselyte/javacore/practice/mycollection_2/FixedQueueRunner.java
+++ src/main/java/net/proselyte/javacore/practice/mycollection_2/FixedQueueRunner.java
@@ -1,12 +1,23 @@
 package net.proselyte.javacore.practice.mycollection_2;
 
+import java.util.NoSuchElementException;
 import java.util.PriorityQueue;
 import java.util.Queue;
+
 public class FixedQueueRunner {
     public static void main(String[] args) {
-        Queue fq = new FixedQueue(10);
+        try{
+        Queue fq = new FixedQueue(-10);
         for(int i = 1; i < 20;i++ )
             fq.offer(i);
         System.out.println(fq);
     }
+    catch (NoSuchElementException e){
+        System.out.println("This is "+e.getClass().getSimpleName() +
+                        ". The capacity will be a number > 0 !"
+                );
+    }
+    }
 }
+
+//    This is NoSuchElementException. The capacity will be a number > 0 !
