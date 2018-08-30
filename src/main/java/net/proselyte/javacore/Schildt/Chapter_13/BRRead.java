1963c5ca5cff15a7fe250e8f24a5ddd9a62199d3
Vladymir <vladymirsheyko@gmail.com>
2018-08-30 16:32:19 +0300

    reading console input

--- src/main/java/net/proselyte/javacore/Schildt/Chapter_13/BRRead.java
+++ src/main/java/net/proselyte/javacore/Schildt/Chapter_13/BRRead.java
@@ -5,7 +5,7 @@ package net.proselyte.javacore.Schildt.Chapter_13;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.SQLOutput;
+
 
 public class BRRead {
     public static void main(String[] args) throws IOException {
