1c19cb3520f14c1534d9cfa3569d39136a8fca72
Vladymir <vladymirsheyko@gmail.com>
2018-09-01 02:28:41 +0300

    lambda expressions and variable capture

---
+++ src/main/java/net/proselyte/javacore/Schildt/Chapter_15/VarCapture.java
@@ -0,0 +1,25 @@
+package net.proselyte.javacore.Schildt.Chapter_15;
+
+// An example of capturing a local variable from the enclosing scope.
+interface MyFunc {
+    int func(int n);
+}
+class VarCapture {
+    public static void main(String args[])
+    {
+// A local variable that can be captured.
+        int num = 10;
+        MyFunc myLambda = (n) -> {
+// This use of num is OK. It does not modify num.
+            int v = num + n;
+
+
+// However, the following is illegal because it attempts to modify the value of num.
+//            num++;  // ERROR !!!
+            return v;
+        };
+// The following line would also cause an error, because it would remove the effectively final status from num.
+//        num = 9;
+//
+    }
+}
