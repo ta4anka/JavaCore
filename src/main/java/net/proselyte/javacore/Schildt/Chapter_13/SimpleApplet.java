99f7eb90d1385f36b0337a15eb7af143bde4b594
Vladymir <vladymirsheyko@gmail.com>
2018-08-31 21:17:08 +0300

    a simple applet

---
+++ src/main/java/net/proselyte/javacore/Schildt/Chapter_13/SimpleApplet.java
@@ -0,0 +1,9 @@
+package net.proselyte.javacore.Schildt.Chapter_13;
+
+import java.awt.*;
+import java.applet.*;
+public class SimpleApplet extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("A Simple Applet", 10, 20);
+    }
+}
