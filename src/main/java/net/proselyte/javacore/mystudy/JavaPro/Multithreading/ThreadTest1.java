38b0d41d415cf75e8159b2d4bf42b1d792b8aae3
Vladymir <vladymirsheyko@gmail.com>
2018-09-24 22:36:53 +0300

    an example of creating thread

--- src/main/java/net/proselyte/javacore/mystudy/JavaPro/Multithreading/ThreadTest1.java
+++ src/main/java/net/proselyte/javacore/mystudy/JavaPro/Multithreading/ThreadTest1.java
@@ -2,13 +2,13 @@ package net.proselyte.javacore.mystudy.JavaPro.Multithreading;
 
 public class ThreadTest1 {
     public static void main(String[] args){
-        MyHtread myHtread = new MyHtread();
+        MyThread myHtread = new MyThread();
         myHtread.start();
         System.out.println("The main thread"); //it's code from main thread
     }
 }
 
-class MyHtread extends Thread {
+class MyThread extends Thread {
     public void run() {
         for(int i = 0; i<10; i++){
             System.out.println("An another thread");
