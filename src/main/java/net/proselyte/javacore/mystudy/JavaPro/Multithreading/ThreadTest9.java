5f00b8a91742f46fb7a60abbcc818a4d86da4002
Vladymir <vladymirsheyko@gmail.com>
2018-10-01 01:21:07 +0300

    an example of wait() and notify()

--- src/main/java/net/proselyte/javacore/mystudy/JavaPro/Multithreading/ThreadTest9.java
+++ src/main/java/net/proselyte/javacore/mystudy/JavaPro/Multithreading/ThreadTest9.java
@@ -1,5 +1,7 @@
 package net.proselyte.javacore.mystudy.JavaPro.Multithreading;
 
+import java.util.Scanner;
+
 public class ThreadTest9 {
     public static void main(String[] args) throws InterruptedException {
         WaitAndNotify wn = new WaitAndNotify();
@@ -7,31 +9,53 @@ public class ThreadTest9 {
         Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
-                wn.produce();
+                try {
+                    wn.produce();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
         Thread thread2 = new Thread(new Runnable() {
             @Override
             public void run() {
-                wn.consume();
+                try {
+                    wn.consume();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
         thread1.start();
         thread2.start();
+
         thread1.join();
         thread2.join();
 
     }
 }
 
+
 class WaitAndNotify{
-    public void produce(){
+    public void produce() throws InterruptedException {
+        synchronized (this){
+            System.out.println("Producer thread started...");
+            wait(); // this.wait() 1 - give intrinsic lock; 2  - wait notify();
+            System.out.println("Producer thread resumed...");
+        }
 
     }
 
-    public void consume(){
+    public void consume() throws InterruptedException {
+        Thread.sleep(2000);
+        Scanner scanner = new Scanner(System.in);
+        synchronized (this){
+            System.out.println("Waiting for return key pressed");
+            scanner.nextLine();
+            notify();
+        }
 
     }
 }
